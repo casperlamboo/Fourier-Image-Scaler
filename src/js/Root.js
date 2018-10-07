1d61fd4730752de57dd358ffdbf176f9bf4b5810
Casper Lamboo <casperlamboo@gmail.com>
2018-10-07 08:52:39 +0200

    use hash router

--- src/js/Root.js
+++ src/js/Root.js
@@ -1,18 +1,18 @@
 import PropTypes from 'proptypes';
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Index from 'js/components/Index.js';
 import Rating from 'js/components/Rating.js';
 import Finished from 'js/components/Finished.js';
 
 const Root = () => (
-  <BrowserRouter>
+  <HashRouter>
     <span>
       <Route exact path="/" component={Index} />
       <Route path="/rate" component={Rating} />
       <Route path="/finished" component={Finished} />
     </span>
-  </BrowserRouter>
+  </HashRouter>
 )
 
 export default Root;
